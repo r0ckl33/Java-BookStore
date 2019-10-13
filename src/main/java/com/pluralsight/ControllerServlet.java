5e713548be1c71b482955ca215c692a316a796ae
r0ckl33 <43154583+r0ckl33@users.noreply.github.com>
2019-10-13 14:01:29 -0400

    Section 1 complete

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -58,6 +58,9 @@ public class ControllerServlet extends HttpServlet {
           break;
 				case "/insert":
 					insertBook(request, response);
+		  break;
+		  case "/delete":
+					deleteBook(request, response);
           break;
         default:
 				   listBooks(request, response);
@@ -102,6 +105,13 @@ public class ControllerServlet extends HttpServlet {
 		Book newBook = new Book(title, author, Float.parseFloat(priceString));
 
 		bookDAO.insertBook(newBook);
+		response.sendRedirect("list");
+	}
+
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		bookDAO.deleteBook(id);
+
 		response.sendRedirect("list");
 	}
 
